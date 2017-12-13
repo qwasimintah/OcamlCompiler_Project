c7f10e25c301f01a1c8f416c29471e5b7ec11a64
Dennis Mintah Djan <djand@F202-10.e-im2ag.ujf-grenoble.fr>
2017-12-13 16:57:53 +0100

    More basic generators

--- java/backend/ArmGenerator.java
+++ java/backend/ArmGenerator.java
@@ -3,58 +3,152 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package backend.arm;
 
 /**
  *
  * @author DJAN DENNIS MINTAH
  */
 public class ArmGenerator {
-    
+
     DataSection dataSection;
     TextSection textSection;
     private int HEAP_SIZE=1024*4; // Heap size in bytes
-    
+
     public ArmGenerator(){
-    
+        //initialise the .data
         dataSection=new DataSection();
-        dataSection.data.append("\t .data");
-      
-        
+        dataSection.data.append("\t.data\n");
+
+        //initialise the .text
         textSection= new TextSection();
-        textSection.text.append("\t .text\n");
-        textSection.text.append("\t .global _start\n");
-        
+        textSection.text.append("\t.text\n");
+        textSection.text.append("\t.global _start\n");
+        textSection.text.append("_start:\n");
+
     }
-    
-    
+
+
+   public void generate_code(List<Functions>  functions){
+
+        //loop through all the available functions 
+        for()
+
+
+   }
+
+
+   /* Offset with imemediate value as offset*/
    private void offset_operations(String mnemonic, String dest, String src, int offset){
-   
-        dataSection.data.append("\t").append(mnemonic).append(" ").append(dest).append("[")
+
+        textSection.text.append("\t").append(mnemonic).append(" ").append(dest).append("[")
                         .append(src).append(" ,#").append(offset).append(" ]\n");
    }
-   
+
+
+   /*offset mode with register as offset*/
+   private void offset_operations(String mnemonic, String dest, String src, String offset){
+
+        textSection.text.append("\t").append(mnemonic).append(" ").append(dest).append("[")
+                        .append(src).append(" ,#").append(offset).append(" ]\n");
+   }
+
+
    private void move_operations(String dest, String src){
-   
-        dataSection.data.append("\t ").append("MOV ").append(dest).append(", ").append(src);
-   
+
+        textSection.text.append("\t").append("MOV ").append(dest).append(", ")
+        .append(src).append("\n");
    }
-   
-   
-   public void assign(){
-   
-   
-   
+
+
+   public void assign(String dest, String src){
+
+      textSection.text.append("\tLDR ").append(dest).append(", ").append(src).append("\n");
+
    }
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-    
+
+
+
+   private void assign(String dest, int src ){
+
+        textSection.text.append("\tLDR ").append(dest).append(", =").append(src).append("\n");
+   }
+
+
+   /*
+   @des: This function genererates code for the arithmetic operations with register
+   as first operand and a intermediate value(INT)
+   @input: mnemonic: ADD/SUB dest: register operand1: register operand2: register
+   */
+
+   public void arith_operation(String mnemonic, String dest, String operand1, String operand2){
+
+        textSection.text.append("\t").append(mnemonic).append(" ").append(dest).append(", ")
+                        .append(operand1).append(", ").append(operand2).append("\n");
+   }
+
+   /*
+   @des: This function genererates code for the arithmetic operations with register
+   as first operand and a intermediate value(INT)
+   @input: mnemonic: ADD/SUB dest: register operand1: register operand2: int
+   */
+   public void arith_operation(String mnemonic, String dest, String operand1, int operand2){
+
+        textSection.text.append("\t").append(mnemonic).append(" ").append(dest).append(", ")
+                        .append(operand1).append(", #").append(operand2).append("\n");
+   }
+
+   /*
+   @des: This function genererates code for the arithmetic operations with register
+   as first operand and a intermediate value(INT)
+   @input: mnemonic: ADD/SUB dest: register operand1: int operand2: register
+   */
+
+   public void arith_operation(String mnemonic, String dest, int operand1, String operand2){
+
+        textSection.text.append("\t").append(mnemonic).append(" ").append(dest).append(", #")
+                        .append(operand1).append(", ").append(operand2).append("\n");
+   }
+
+   /*
+   @des: This function genererates code for the arithmetic operations with register
+   as first operand and a intermediate value(INT)
+   @input: mnemonic: ADD/SUB dest: register operand1: int operand2: int
+   */
+
+   public void arith_operation(String mnemonic, String dest, int operand1, int operand2){
+
+        textSection.text.append("\t").append(mnemonic).append(" ").append(dest).append(", #")
+                        .append(operand1).append(", #").append(operand2).append("\n");
+   }
+
+
+
+
+
+
+   public static void main(String[] args){
+
+        ArmGenerator  arm = new ArmGenerator();
+
+        arm.move_operations("r4", "r5");
+        arm.arith_operation("ADD","r5","r6", "r7");
+        arm.arith_operation("SUB","r5",1, "r7");
+        arm.arith_operation("ADD","r5","r6", 1);
+        arm.assign("r5",256);
+
+        StringBuilder result= arm.textSection.text;
+
+        System.out.println(result);
+
+   }
+
+
+
+
+
+
+
+
+
+
 }
