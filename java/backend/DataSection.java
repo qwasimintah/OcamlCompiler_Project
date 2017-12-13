8ddbbad469020c9b2d99c5906870a4b32477a010
Dennis Mintah Djan <djand@F202-10.e-im2ag.ujf-grenoble.fr>
2017-12-13 12:09:19 +0100

    Added basic arm generator functions

---
+++ java/backend/DataSection.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author DJAN DENNIS MINTAH
+ */
+public class DataSection {
+    
+    public static StringBuilder data;
+    
+    public DataSection(){
+      data=new StringBuilder();
+    }
+    
+    
+    
+}
