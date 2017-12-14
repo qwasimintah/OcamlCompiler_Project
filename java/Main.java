7c54050eed115648006dec8aca1705af2acf798c
rayard <remi.balsamo@hotmail.fr>
2017-12-14 14:44:17 +0100

    KNormalization finished

--- java/Main.java
+++ java/Main.java
@@ -1,6 +1,7 @@
 import java_cup.runtime.*;
 import java.io.*;
 import java.util.*;
+import frontend.*;
 
 public class Main {
   static public void main(String argv[]) {
@@ -24,6 +25,13 @@ public class Main {
       /* For evaluation :
       System.out.println("------ Evaluation ------");
       System.out.println("Ceci est le résultat : " + expression.accept(new EvaluationVisitor()));
+      */
+
+      /* For KNormalization :
+      System.out.println("------ K-Normalization ------");
+      Exp expression_normalized = expression.accept(new KNormalization());
+      System.out.println("Ceci est le résultat : ");
+      expression_normalized.accept(new PrintVisitor());
       */
 
     } catch (Exception e) {
