6f0aad94f54449bf9310d38a39d39648ca6584f0
Fabien Tribel <fabien.tribel@gmail.com>
2017-12-14 19:40:06 +0100

    changes

--- java/backend/functions/Function.java
+++ java/backend/functions/Function.java
@@ -8,6 +8,7 @@ public class Function {
 private List<Instruction> instructions;
 private List<Variable> arguments;
 private Integer spillOffset = 0;
+private HashSet<Variable> variables = new HashSet<Variable>();
 
 public Function(List<Variable> arguments, List<Instruction> instructions) {
         this.instructions = instructions;
@@ -20,6 +21,18 @@ public List<Instruction> getInstructions() {
 
 public void addInstruction(Instruction instruction) {
         instructions.add(instruction);
+
+        for (Object v : instruction.getOperands()) {
+                if (v instanceof Variable) {
+                        variables.add((Variable) v);
+                } else if (v instanceof Instruction) {
+                        for (Object x : ((Instruction)v).getOperands()) {
+                                if (x instanceof Variable) {
+                                        variables.add((Variable) x);
+                                }
+                        }
+                }
+        }
 }
 
 public Iterator<Instruction> iterator() {
@@ -41,6 +54,12 @@ public void setOffset(Integer value) {
 public void show() {
         for (Instruction i : instructions) {
                 i.show();
+        }
+}
+
+public void showVariablesState() {
+        for (Variable v : variables) {
+                v.getSaveState();
         }
 }
 }
---
+++ java/backend/registers/RegisterUtils.java
@@ -0,0 +1,29 @@
+package registers;
+
+import java.util.*;
+import registers.*;
+import variables.*;
+
+public class RegisterUtils {
+
+public static void initRegisters(HashMap<Register, Variable> registers) {
+        for (Integer i = 4; i < 13; i++) {
+                Register reg = new Register(i);
+                registers.put(reg, null);
+        }
+}
+
+public static void showRegisters(HashMap<Register, Variable> registers) {
+        System.out.println("------ State of the registers ------");
+        for(Object key: registers.keySet()) {
+                Register reg = (Register) key;
+                System.out.print("r" + reg.getIndex() + "\t:\t");
+                try {
+                        System.out.println(registers.get(reg).getName());
+                } catch (NullPointerException e) {
+                        System.out.println("empty");
+                }
+        }
+        System.out.println("\n");
+}
+}
