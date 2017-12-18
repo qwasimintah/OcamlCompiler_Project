1bc1ff1119b78875cd681b028ad775caeb733223
Dennis Mintah Djan <djand@ensipc58.imag.fr>
2017-12-18 13:10:54 +0100

    Testing complete for variable offset

--- ARM/add.s
+++ ARM/add.s
@@ -7,13 +7,17 @@ _main:
 	SUB sp, #4
 	LDR lr, [sp]
 	SUB sp, #4
-	MOV r4, #5
-	MOV r0, #9
-	STR r0 , [sp , #4]
-	LDR r1 , [sp ,#4]
-	ADD r0, r4, r1
+	STR fp, [sp]
+	MOV fp, sp
+	SUB sp, #4
+	MOV r6, #5
+	MOV r9, #9
+	MUL r0, r9, r6
 	ADD sp, #4
 	 @MAIN EPILOGUE
+	ADD sp, #4
+	MOV sp, fp
+	LDR fp, [sp]
 	ADD sp, #4
 	MOV r7, #1
 	swi 0
