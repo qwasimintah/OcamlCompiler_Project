ba1322f39b22af1b6ef7979ea006702aceaecda8
Quentin Delfosse <q.delfosse@gmail.com>
2017-12-19 14:24:26 +0100

    ajout de tests particulier

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,17 +1,35 @@
 #! /bin/bash
+
 cd "$(dirname "$0")"/.. || exit 1
 MINCAMLC=java/mincamlc
 
+# cd java
+# make clean 2> /dev/null 1> /dev/null
+# make 2> /dev/null 1> /dev/null
+# cd ..
+# clear
+
+if [ $# -eq 0 ]
+  then
+    test_list="ast knorm reduction"
+  else
+    test_list=$1
+fi
 
 input_files=`ls tests/input/`
-for testing_part in 'ast' 'knorm'
+for testing_part in ${test_list}
   do
-    echo -e "\n \033[33m \t \t \t $testing_part \033[0m \n"
-    echo "generation in tests/$testing_part/output/"
-    for file in $input_files
-    do
-          echo -e "generating \033[0;36m ${file:5:-3}.out \033[0m"
-          $MINCAMLC tests/input/$file --$testing_part > tests/$testing_part/output/${file:5:-3}.out
-    done
+    if [ -d "tests/"${testing_part} ]
+      then
+      echo -e "\n \033[33m \t \t \t $testing_part \033[0m \n"
+      echo "generation in tests/$testing_part/output/"
+      for file in $input_files
+      do
+            echo -e "generating \033[0;36m ${file:5:-3}.out \033[0m"
+            $MINCAMLC tests/input/$file --$testing_part > tests/$testing_part/output/${file:5:-3}.out
+      done
+    else
+      echo -e "\033[31m The specified test folder ${testing_part} does not exist \033[0m"
+  fi
   done
 echo ""
--- scripts/test_diff.sh
+++ scripts/test_diff.sh
@@ -1,20 +1,31 @@
 #! /bin/bash
 cd "$(dirname "$0")"/.. || exit 1
 
+if [ $# -eq 0 ]
+  then
+    test_list="ast knorm"
+  else
+    test_list=$1
+fi
 
 input_files=`ls tests/input/`
-for testing_part in 'ast' 'knorm'
+for testing_part in ${test_list}
   do
-    echo -e "\n \033[33m \t \t \t $testing_part \033[0m \n"
-    for file in $input_files
-    do
-        echo -ne "testing parser on: \033[0;36m ${file:5:-3}.out : \033[0;40m"
-        if diff tests/$testing_part/output/${file:5:-3}.out tests/$testing_part/expected/${file:5:-3}.exp 2> /dev/null 1> /dev/null
-        then
-          echo -e '\033[0;32m PASSED \033[0m'
-        else
-          echo -e '\033[31m FAILED \033[0m'
-        fi
-    done
+    if [ -d "tests/"${testing_part} ]
+      then
+      echo -e "\n \033[33m \t \t \t $testing_part \033[0m \n"
+      for file in $input_files
+      do
+          echo -ne "testing parser on: \033[0;36m ${file:5:-3}.out : \033[0;40m"
+          if diff tests/$testing_part/output/${file:5:-3}.out tests/$testing_part/expected/${file:5:-3}.exp 2> /dev/null 1> /dev/null
+          then
+            echo -e '\033[0;32m PASSED \033[0m'
+          else
+            echo -e '\033[31m FAILED \033[0m'
+          fi
+      done
+    else
+      echo -e "\033[31m The specified test folder ${testing_part} does not exist \033[0m"
+    fi
   done
 echo ""
