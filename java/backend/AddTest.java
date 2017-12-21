fba6179c4533b4663d0b18e94ca7bac60504be4f
Dennis Mintah Djan <djand@ensipc60.imag.fr>
2017-12-21 20:09:47 +0100

    Added LetAdd test

--- java/backend/AddTest.java
+++ java/backend/AddTest.java
@@ -1,3 +1,4 @@
+package backend;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
--- java/backend/RegisterAllocation.java
+++ java/backend/RegisterAllocation.java
@@ -1,3 +1,4 @@
+
 import java.io.*;
 import java.util.Hashtable;
 import java.util.*;
