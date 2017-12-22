c261df0fa5b59f845b6e12c0ddd56ecf51cc6b5a
Quentin Delfosse <q.delfosse@gmail.com>
2017-12-22 17:23:50 +0100

    modification du main pour qu'on puisse faire le global (les prints sont dans les conditions)

--- java/Main.java
+++ java/Main.java
@@ -12,77 +12,80 @@ static public void main(String argv[]) {
         // À mettre dans une IHM
         Ihm ihm = new Ihm(argv);
         try {
-                Parser p = new Parser(new Lexer(new FileReader(argv[0])));
-                Exp expression = (Exp) p.parse().value;
-                assert (expression != null);
+              Parser p = new Parser(new Lexer(new FileReader(argv[0])));
+              Exp expression = (Exp) p.parse().value;
+              assert (expression != null);
 
               if (ihm.given_output){
-                // System.out.println("hello;");
-                new Outgesture(ihm.output_file);
+              // System.out.println("hello;");
+              new Outgesture(ihm.output_file);
               }
 
-                if (ihm.ast || ihm.parse_only) {
-                    System.out.println("------ AST ------");
-                    expression.accept(new PrintVisitor());
-                    System.out.println();
+              int height = Height.computeHeight(expression);
 
-                    System.out.println("------ Height of the AST ------");
-                    int height = Height.computeHeight(expression);
-                    System.out.println("using Height.computeHeight: " + height);
+              if (ihm.ast || ihm.parse_only) {
+                  System.out.println("------ AST ------");
+                  expression.accept(new PrintVisitor());
+                  System.out.println();
 
-                    ObjVisitor<Integer> v = new HeightVisitor();
-                    height = expression.accept(v);
-                    System.out.println("using HeightVisitor: " + height);
-                }
+                  System.out.println("------ Height of the AST ------");
+                  System.out.println("using Height.computeHeight: " + height);
+              }
+              ObjVisitor<Integer> v = new HeightVisitor();
+              height = expression.accept(v);
 
-                if (ihm.parse_only) {
-                    System.exit(0);
-                }
-                /* For evaluation :
-                   System.out.println("------ Evaluation ------");
-                   System.out.println("Ceci est le résultat : " + expression.accept(new EvaluationVisitor()));
-                 */
+              if (ihm.ast || ihm.parse_only) {
+                  System.out.println("using HeightVisitor: " + height);
+              }
 
-                // For KNormalization :
-                if (ihm.knorm) {
-                        System.out.println("------ K-Normalization ------");
-                        Exp expression_normalized = expression.accept(new KNormalization());
-                        expression_normalized.accept(new PrintVisitor());
-                        System.out.println("");
-                }
+              if (ihm.parse_only) {
+                  System.exit(0);
+              }
+              /* For evaluation :
+                 System.out.println("------ Evaluation ------");
+                 System.out.println("Ceci est le résultat : " + expression.accept(new EvaluationVisitor()));
+               */
 
-                //For AlphaConversion :
-                if (ihm.alpha_conversion) {
-                        System.out.println("------ AlphaConversion ------");
-                        //Exp expression_normalized = expression.accept(new KNormalization());
-                        //Exp expression_converted = expression_normalized.accept(new AlphaConversion());
-                        Exp expression_converted = expression.accept(new AlphaConversion());
-                        expression_converted.accept(new PrintVisitor());
-                        System.out.println("");
-                }
+              // For KNormalization :
+              Exp expression_normalized = expression.accept(new KNormalization());
+              if (ihm.knorm) {
+                      System.out.println("------ K-Normalization ------");
+                      expression_normalized.accept(new PrintVisitor());
+                      System.out.println("");
+              }
 
-                // For Reduction of Nested Let-Expressions
-                if (ihm.reduction) {
-                        System.out.println("------ Reduction of Nested Let-Expressions ------");
-                        Exp expression_reducted = expression.accept(new ReductionNestedExpression());
-                        expression_reducted.accept(new PrintVisitor());
-                        System.out.println("");
-                }
+              //For AlphaConversion :
+              Exp expression_converted = expression.accept(new AlphaConversion());
+              if (ihm.alpha_conversion) {
+                      System.out.println("------ AlphaConversion ------");
+                      //Exp expression_normalized = expression.accept(new KNormalization());
+                      //Exp expression_converted = expression_normalized.accept(new AlphaConversion());
+                      expression_converted.accept(new PrintVisitor());
+                      System.out.println("");
+              }
 
-                // if (true) {
-                //         System.out.println("------ Translation to Jerry ------");
-                //         TranslationVisitor tv = new TranslationVisitor();
-                //         Function func = new Function("main", new ArrayList(), new ArrayList());
-                //         tv.visit((Exp) expression, func);
-                //         System.out.println("");
-                // }
+              // For Reduction of Nested Let-Expressions
+              Exp expression_reducted = expression.accept(new ReductionNestedExpression());
+              if (ihm.reduction) {
+                      System.out.println("------ Reduction of Nested Let-Expressions ------");
+                      expression_reducted.accept(new PrintVisitor());
+                      System.out.println("");
+              }
 
-                if (ihm.arm){
-                  System.out.println("@------ ARM ------");
-                  Exp expression_reducted = expression.accept(new ReductionNestedExpression());
-                  expression_reducted.accept(new PrintVisitor());
-                  System.out.println("");
-                }
+              // if (true) {
+              //         System.out.println("------ Translation to Jerry ------");
+              //         TranslationVisitor tv = new TranslationVisitor();
+              //         Function func = new Function("main", new ArrayList(), new ArrayList());
+              //         tv.visit((Exp) expression, func);
+              //         System.out.println("");
+              // }
+
+              // Exp expression_reducted = expression.accept(new ReductionNestedExpression());
+              if (ihm.arm){
+                System.out.println("@------ ARM ------");
+                expression_reducted.accept(new PrintVisitor());
+                System.out.println("");
+              }
 
         } catch (Exception e) {
                 e.printStackTrace();
