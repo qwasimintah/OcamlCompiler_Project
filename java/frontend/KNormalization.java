bbd1b96b145314e5a9cd53d1e0781d5181656066
rayard <remi.balsamo@gmail.com>
2018-01-16 17:26:24 +0100

    Good knorm for tuple

--- java/frontend/KNormalization.java
+++ java/frontend/KNormalization.java
@@ -73,26 +73,34 @@ public Exp visit(App e) {
 }
 
 public Exp visit(Tuple e) {
-  List<Let> list_let = new ArrayList<Let>();
-  List<Exp> list_var = new ArrayList<Exp>();
-  for (int j = 0; j < e.es.size(); j++){
-    list_var.add(new Var(id_generator.gen()));
-    Var new_var = (Var) list_var.get(j);
-    list_let.add(new Let(new_var.id, new TInt(), new Int(1), new Int(1)));
-  }
-  for (int i = (e.es.size() - 1); i >= 0; i--){
-    Var new_var = (Var) list_var.get(i);
-    Type t = new TInt();
+  List<Exp> list_es = new ArrayList<Exp>();
+
+  for (int i = 0; i < e.es.size(); i++){
     Exp es_temp = e.es.get(i);
-    if (i < e.es.size() - 1){
-      list_let.set(i, new Let(new_var.id, t, es_temp.accept(this), list_let.get(i+1)));
-    }
-    else {
-      Tuple new_tuple = new Tuple(list_var);
-      list_let.set(i, new Let(new_var.id, t, es_temp.accept(this), new_tuple));
-    }
+    list_es.add(es_temp.accept(this));
   }
-  return list_let.get(0);
+  return new Tuple(list_es);
+
+  // List<Let> list_let = new ArrayList<Let>();
+  // List<Exp> list_var = new ArrayList<Exp>();
+  // for (int j = 0; j < e.es.size(); j++){
+  //   list_var.add(new Var(id_generator.gen()));
+  //   Var new_var = (Var) list_var.get(j);
+  //   list_let.add(new Let(new_var.id, new TInt(), new Int(1), new Int(1)));
+  // }
+  // for (int i = (e.es.size() - 1); i >= 0; i--){
+  //   Var new_var = (Var) list_var.get(i);
+  //   Type t = new TInt();
+  //   Exp es_temp = e.es.get(i);
+  //   if (i < e.es.size() - 1){
+  //     list_let.set(i, new Let(new_var.id, t, es_temp.accept(this), list_let.get(i+1)));
+  //   }
+  //   else {
+  //     Tuple new_tuple = new Tuple(list_var);
+  //     list_let.set(i, new Let(new_var.id, t, es_temp.accept(this), new_tuple));
+  //   }
+  // }
+  // return list_let.get(0);
 }
 
 public Exp visit(If e) {
